168ebbdd572cf9fa376e6352234b0d2dd9b2a3a6
Nicolas Bureš <nick.bures@gmail.com>
2022-03-07 13:05:21 +0100

    Revert "Přidáná funkcionalita tips pomocí buttons"
    This reverts commit a1112780af43c07d21ac2782799eaad2a1005d72.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const form = document.querySelector('form')
 const inputBill = document.querySelector('#bill-input')
 const inputPeople = document.querySelector('#people-input')
 const btnTip = document.querySelectorAll('#tips-btns button')
-const tipInput = document.querySelector('#tip-input')
 const tipAm = document.querySelector('#tip-amount')
 const totalPr = document.querySelector('#total-price')
 const reset = document.querySelector('#reset')
@@ -10,7 +9,6 @@ const error = document.querySelector('.error')
 let bill = 0
 let peoples = 1
 let customTips = 0
-let tip = 0
 
 inputBill.addEventListener('keyup', (e) => {
     bill = inputBill.value
@@ -30,34 +28,105 @@ inputPeople.addEventListener('keyup', (e) => {
     CalTotal()
 });
 
+
+
+
+
+
+
+
 for (const button of btnTip ){
     button.addEventListener('click', (e) => {
         e.preventDefault()
         let btn = Array.from(btnTip)
         if(button === btn[0]){
-            tip = bill / peoples * 0.05         
-        }else if (button === btn[1]){
-            tip = bill / peoples * 0.10    
-        }else if (button === btn[2]){
-            tip = bill / peoples * 0.15 
-        }else if (button === btn[3]){
-            tip = bill / peoples * 0.25    
+            return bill / peoples * 0.05
+            
         }else {
-            tip = bill / peoples * 0.50    
+            console.log("nefunguje");
         }
-        CalTotal()
-    })    
+        
+    })
+   
 }
 
-tipInput.addEventListener('change', (e) => {
-    console.log(tipInput.value);
-})
+
+
 
 
 
 const CalTotal = () => {
-    let totalDiv = bill / peoples 
-    let tipDiv = tip
-    totalPr.innerHTML = "$"+ totalDiv.toFixed(2)
-    tipAm.innerHTML = "$"+ tipDiv.toFixed(2)
+    let tipDiv = bill / peoples 
+    totalPr.innerHTML = "$"+ tipDiv.toFixed(2)
 }
+
+
+
+
+
+
+
+
+
+
+
+//Error message
+// inputPeople.addEventListener('change', (e) => {
+//     if(inputPeople.value === "0" || inputPeople.value === ""){
+//         error.classList.remove('hidden')
+//         inputPeople.style.border = "2px solid red"
+//     }else{
+//         peoples = inputPeople.value
+//         error.classList.add('hidden')
+//         inputPeople.style.border = ""
+        
+//     }
+
+//     console.log(inputPeople.value);
+// })
+
+
+// for (const button of btnTip) {
+//     btnTip.addEventListener('submit', (e) => {
+//         e.preventDefault()
+
+//     })
+// }
+
+
+// const calc = (tip, total) => {
+//     if (tip === "0"){
+//         return (total - bill)
+//     } else {
+//         for (const button of btnTip) {
+//             if (button.classList.item(0) === tip){
+//                 button.classList.add("active")
+//             } else {
+//                 button.classList.remove("active")
+//             }
+//         }
+
+//         switch (tip) {
+//             case "btn5":
+//               total = bill * 0,05
+//             break;
+//             case "btn10":
+//                 total = bill * 0,1
+//             break;
+//             case "btn15":
+//                 total = bill * 0,15
+//             break;
+//             case "btn25":
+//                 total = bill * 0,25
+//             break;
+//             case "btn50":
+//                 total = bill * 0,50
+//             break;
+//             default:
+//                 console.log(tip)
+//                 // total = total + total*tip*0.01;
+//                 break;
+//         }
+
+//     }
+// }
