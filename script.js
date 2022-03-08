0099b39e9aaf4029bb475bc8b1b30b16a11f11e9
Nicolas Bureš <nick.bures@gmail.com>
2022-03-08 10:52:36 +0100

    Vrácení do funkčního stavu

--- script.js
+++ script.js
@@ -2,23 +2,31 @@ const form = document.querySelector('form')
 const inputBill = document.querySelector('#bill-input')
 const inputPeople = document.querySelector('#people-input')
 const btnTip = document.querySelectorAll('#tips-btns button')
+const tipInput = document.querySelector('#tip-input')
 const tipAm = document.querySelector('#tip-amount')
 const totalPr = document.querySelector('#total-price')
-const reset = document.querySelector('#reset')
 const error = document.querySelector('.error')
+const reset = document.querySelector('#reset')
+reset.disabled =true
+
 let bill = 0
 let peoples = 1
 let customTips = 0
+let tip = 0
 
+
+//Zachycení celkové hodnoty
 inputBill.addEventListener('keyup', (e) => {
     bill = inputBill.value
-    bill === "" ? bill = 0 : bill = parseFloat(bill)
+    reset.disabled = false;
     CalTotal()
 })
 
+//Zachycení počtu lidí
 inputPeople.addEventListener('keyup', (e) => {
     peoples = inputPeople.value
-    if (peoples === "" || peoples === 0) {
+    reset.disabled = false;
+    if (peoples === "" || peoples === "0") {
         error.classList.remove('hidden')
         inputPeople.style.border = "2px solid red"
     } else {
@@ -28,105 +36,53 @@ inputPeople.addEventListener('keyup', (e) => {
     CalTotal()
 });
 
-
-
-
-
-
-
-
+//Button's tip
 for (const button of btnTip ){
     button.addEventListener('click', (e) => {
         e.preventDefault()
         let btn = Array.from(btnTip)
         if(button === btn[0]){
-            return bill / peoples * 0.05
-            
+            tip = bill / peoples * 0.05         
+        }else if (button === btn[1]){
+            tip = bill / peoples * 0.10     
+        }else if (button === btn[2]){
+            tip = bill / peoples * 0.15 
+        }else if (button === btn[3]){
+            tip = bill / peoples * 0.25    
         }else {
-            console.log("nefunguje");
+            tip = bill / peoples * 0.50    
         }
-        
-    })
-   
+        CalTotal()
+    })    
 }
 
+//Input tip
+tipInput.addEventListener('keyup', (e) => {
+    if(tipInput.value) {
+        tip = tipInput.value / 100 * bill
+    }
+    CalTotal()
+})
 
 
-
-
-
+//Funkce na vypisování výsledku
 const CalTotal = () => {
-    let tipDiv = bill / peoples 
-    totalPr.innerHTML = "$"+ tipDiv.toFixed(2)
+    let totalDiv = bill / peoples 
+    let tipDiv = tip
+    totalPr.innerHTML = "$"+ totalDiv.toFixed(2)
+    tipAm.innerHTML = "$"+ tipDiv.toFixed(2)
 }
 
+//Nastavení tlačítka return
 
 
 
-
-
-
-
-
-
-
-//Error message
-// inputPeople.addEventListener('change', (e) => {
-//     if(inputPeople.value === "0" || inputPeople.value === ""){
-//         error.classList.remove('hidden')
-//         inputPeople.style.border = "2px solid red"
-//     }else{
-//         peoples = inputPeople.value
-//         error.classList.add('hidden')
-//         inputPeople.style.border = ""
-        
-//     }
-
-//     console.log(inputPeople.value);
-// })
-
-
-// for (const button of btnTip) {
-//     btnTip.addEventListener('submit', (e) => {
-//         e.preventDefault()
-
-//     })
-// }
-
-
-// const calc = (tip, total) => {
-//     if (tip === "0"){
-//         return (total - bill)
-//     } else {
-//         for (const button of btnTip) {
-//             if (button.classList.item(0) === tip){
-//                 button.classList.add("active")
-//             } else {
-//                 button.classList.remove("active")
-//             }
-//         }
-
-//         switch (tip) {
-//             case "btn5":
-//               total = bill * 0,05
-//             break;
-//             case "btn10":
-//                 total = bill * 0,1
-//             break;
-//             case "btn15":
-//                 total = bill * 0,15
-//             break;
-//             case "btn25":
-//                 total = bill * 0,25
-//             break;
-//             case "btn50":
-//                 total = bill * 0,50
-//             break;
-//             default:
-//                 console.log(tip)
-//                 // total = total + total*tip*0.01;
-//                 break;
-//         }
-
-//     }
-// }
+reset.addEventListener('click', (e) => {
+    e.preventDefault()
+    CalTotal ()
+    inputBill.value = 0
+    inputPeople.value = 0
+    totalPr.innerHTML = "$0.00"
+    tipAm.innerHTML = "$0.00"
+    reset.disabled = true
+})
